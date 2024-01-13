f8d7c6135925c505b9598e1c3e29e7af02f4fd2e
leith schmidtlein <Leithds@byui.edu>
2024-01-13 15:57:07 -0700

    4th down

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        int number;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        int sum = 0;
+
+        int largestNumber = int.MinValue;
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+
+            if (num > largestNumber)
+            {
+                largestNumber = num;
+            }
+        }
+
+        float average = (float)sum / numbers.Count;
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Largest Number: {largestNumber}");
+        Console.WriteLine($"Average: {average}");
     }
 }
