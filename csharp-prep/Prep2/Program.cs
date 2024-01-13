ea053933e57aa0c27c285743e3cddd22f7a5deab
leith schmidtlein <Leithds@byui.edu>
2024-01-13 14:52:34 -0700

    2nd one done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse(gradePercentage);
+        
+        if (percentage >= 90)
+        {
+            Console.WriteLine("You have an A in the class! ");
+        }
+        else if(percentage >= 80)
+        {
+            Console.WriteLine("You have a B in the class!");
+        }
+        else if(percentage >= 70)
+        {
+            Console.WriteLine("You have a C in the class.");
+        }
+        else if(percentage >= 60)
+        {
+            Console.WriteLine("You have a D in the class...");
+        }
+        else if(percentage < 60)
+        {
+            Console.WriteLine("You got an F you loser! ");
+        }
     }
 }
