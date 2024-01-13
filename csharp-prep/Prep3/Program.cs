d37f648e0b7c2c73bccbc73667f64d098f2ceccf
leith schmidtlein <Leithds@byui.edu>
2024-01-13 15:30:10 -0700

    please work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess;
+
+        do
+        {
+            Console.Write("What is the magic number? ");
+            string userImput = Console.ReadLine();
+            guess = int.Parse(userImput);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+        } while (guess != number);
+
+        Console.WriteLine($"Good job! You guessed {number} correctly!");
     }
 }
