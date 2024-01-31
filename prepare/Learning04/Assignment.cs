c4e82482409f4ec286080e940987ec045a8fa305
Leith Schmidtlein <leithds@byui.edu>
2024-01-31 16:53:11 -0700

    getting it done

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Assignment
+{
+    private string _studentName = "";
+    private string _topic = "";
+
+    public Assignment()
+    {
+        _studentName = "Anonymous";
+        _topic = "Unknown";
+    }
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return _studentName + " " + _topic;
+    }
+}
